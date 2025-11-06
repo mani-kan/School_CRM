5e4337cfca28f3db3ce60b0a697f5583574aa001
mani-kan <mani1873gmk@gmail.com>
2025-11-06 20:02:09 +0530

    initial commit

---
+++ school/teachers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from. import views
+urlpatterns = [
+    path('',views.teacher_list,name='all-teacher'),
+    path('add-teacher/',views.add_teacher,name="add-teacher"),
+    path('update-teacher/<str:id>',views.update_teacher,name='update-teacher'),
+    path('delete/<str:id>',views.delete_teacher,name="delete")
+]
